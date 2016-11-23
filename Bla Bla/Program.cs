169091c874f8153652d5bc83effcb58e02945d47
Ivka <Ivka@DESKTOP-38HTR02>
2016-11-23 16:10:06 +0200

    hhjklh

---
+++ Bla Bla/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bla_Bla
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("After midnight the cols aren't funny!");
+        }
+    }
+}
